db9c4296858e486ff3c9fca7488a1bf1410549e6
Gene Bogdanovich <genebogdanovich@gmail.com>
2024-11-13 13:56:42 +0300

    Initial Commit

---
+++ HorizontalOverflowResolution/HorizontalOverflowResolutionApp.swift
@@ -0,0 +1,17 @@
+//
+//  HorizontalOverflowResolutionApp.swift
+//  HorizontalOverflowResolution
+//
+//  Created by Gene Bogdanovich on 13.11.24.
+//
+
+import SwiftUI
+
+@main
+struct HorizontalOverflowResolutionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
